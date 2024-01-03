5de77f4b21ab7a44fb52cf84b91a10130507e606
unknown <jirrazu123@gmail.com>
2024-01-04 00:42:21 +0600

    counter normal

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <script setup>
+import { ref } from "vue";
 
+const counter = ref(0);
 </script>
 
 <template>
   <div class="h-screen flex items-center justify-center">
-      <div class="bg-white border-1 rounded-md shadow-md w-full max-w-xs">
-          <h1 class="text-center text-3xl">Counter</h1>
-          <div class="m-2 p-5">
-            <div class="text-center py-4 border-2">
-          <p class="text-indigo-400 font-semibold text-3xl">0</p>
-           
+    <div class="bg-white border-2 rounded-md shadow-lg  w-full max-w-xs">
+      <h1 class="text-center text-indigo-500 text-3xl pt-2">Counter</h1>
+      <div class="m-2 p-5">
+        <!--Counter-->
+        <div class="text-center py-4 border-2">
+          <p class="text-indigo-400 font-semibold text-3xl">{{ counter }}</p>
         </div>
-        <!--email and phone button-->
+        <!--Counter end-->
+
+        <!--Decrease button-->
         <div class="flex mt-5 border-t">
-          <a href="#" class="text-gray-400 text-3xl flex items-center justify-center border w-1/2 py-3 hover:bg-blue-100">
-            -</a>
+          <button @click="counter--"
+            class="text-gray-400 text-3xl flex items-center justify-center border w-1/2 py-3 hover:bg-blue-100"
+          >
+            -</button
+          >
+          <!--Decrease button end-->
 
-            <a href="#" class="text-gray-400 text-3xl flex items-center justify-center border w-1/2 py-3 hover:bg-blue-100 ">
-            +</a>
-
+          <!--Increase button-->
+          <button @click="counter++"
+            class="text-gray-400 text-3xl flex items-center justify-center border w-1/2 py-3 hover:bg-blue-100"
+          >
+            +</button
+          >
+          <!--Increase button end-->
         </div>
-          </div>
       </div>
+    </div>
   </div>
 </template>
