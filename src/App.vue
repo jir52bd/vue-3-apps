45efbd9e03a6a89c268ea697f14ddcf27c0efd4d
unknown <jirrazu123@gmail.com>
2024-01-04 00:26:43 +0600

    Counter design part

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,24 @@
 </script>
 
 <template>
-  <div>
-    Vue3 apps
+  <div class="h-screen flex items-center justify-center">
+      <div class="bg-white border-1 rounded-md shadow-md w-full max-w-xs">
+          <h1 class="text-center text-3xl">Counter</h1>
+          <div class="m-2 p-5">
+            <div class="text-center py-4 border-2">
+          <p class="text-indigo-400 font-semibold text-3xl">0</p>
+           
+        </div>
+        <!--email and phone button-->
+        <div class="flex mt-5 border-t">
+          <a href="#" class="text-gray-400 text-3xl flex items-center justify-center border w-1/2 py-3 hover:bg-blue-100">
+            -</a>
+
+            <a href="#" class="text-gray-400 text-3xl flex items-center justify-center border w-1/2 py-3 hover:bg-blue-100 ">
+            +</a>
+
+        </div>
+          </div>
+      </div>
   </div>
 </template>
-
-<style scoped>
-
-</style>
